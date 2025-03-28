b45b18af14625dfeaea2ae365696f048fb1b8dc6
Mukkun <fitahianalova@gmail.com>
2025-03-28 07:08:35 +0300

    Initial commit

---
+++ src/assets/js/events.js
@@ -0,0 +1,124 @@
+import helpers from './helpers.js';
+
+
+window.addEventListener( 'load', () => {
+    //When the chat icon is clicked
+    document.querySelector( '#toggle-chat-pane' ).addEventListener( 'click', ( e ) => {
+        let chatElem = document.querySelector( '#chat-pane' );
+        let mainSecElem = document.querySelector( '#main-section' );
+
+        if ( chatElem.classList.contains( 'chat-opened' ) ) {
+            chatElem.setAttribute( 'hidden', true );
+            mainSecElem.classList.remove( 'col-md-9' );
+            mainSecElem.classList.add( 'col-md-12' );
+            chatElem.classList.remove( 'chat-opened' );
+        }
+
+        else {
+            chatElem.attributes.removeNamedItem( 'hidden' );
+            mainSecElem.classList.remove( 'col-md-12' );
+            mainSecElem.classList.add( 'col-md-9' );
+            chatElem.classList.add( 'chat-opened' );
+        }
+
+        //remove the 'New' badge on chat icon (if any) once chat is opened.
+        setTimeout( () => {
+            if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
+                helpers.toggleChatNotificationBadge();
+            }
+        }, 300 );
+    } );
+
+
+    //When the video frame is clicked. This will enable picture-in-picture
+    document.getElementById( 'local' ).addEventListener( 'click', () => {
+        if ( !document.pictureInPictureElement ) {
+            document.getElementById( 'local' ).requestPictureInPicture()
+                .catch( error => {
+                    // Video failed to enter Picture-in-Picture mode.
+                    console.error( error );
+                } );
+        }
+
+        else {
+            document.exitPictureInPicture()
+                .catch( error => {
+                    // Video failed to leave Picture-in-Picture mode.
+                    console.error( error );
+                } );
+        }
+    } );
+
+
+    //When the 'Create room" is button is clicked
+    document.getElementById( 'create-room' ).addEventListener( 'click', ( e ) => {
+        e.preventDefault();
+
+        let roomName = document.querySelector( '#room-name' ).value;
+        let yourName = document.querySelector( '#your-name' ).value;
+        let yourMdp = document.querySelector( '#your-mdp' ).value;
+
+        if ( roomName && yourName && yourMdp == "webrtcvisio" ) {
+            //remove error message, if any
+            document.querySelector( '#err-msg' ).innerHTML = "";
+
+            //save the user's name in sessionStorage
+            sessionStorage.setItem( 'username', yourName );
+
+            //create room link
+            let roomLink = `${ location.origin }?room=${ roomName.trim().replace( ' ', '_' ) }_${ helpers.generateRandomString() }`;
+
+            //show message with link to room
+            document.querySelector( '#room-created' ).innerHTML = `Le Room a ete creer avec succes.Veuliiez clique  <a href='${ roomLink }'>ici</a> pour y acceder. 
+                Partager le lien du room avec les autre participants.`;
+
+            //empty the values
+            document.querySelector( '#room-name' ).value = '';
+            document.querySelector( '#your-name' ).value = '';
+            document.querySelector( '#your-mdp' ).value = '';
+        }
+
+        else {
+            document.querySelector( '#err-msg' ).innerHTML = "Mot de passe incorecte";
+        } 
+    } );
+
+
+    //When the 'Enter room' button is clicked.
+    document.getElementById( 'enter-room' ).addEventListener( 'click', ( e ) => {
+        e.preventDefault();
+
+        let name = document.querySelector( '#username' ).value;
+
+        if ( name ) {
+            //remove error message, if any
+            document.querySelector( '#err-msg-username' ).innerHTML = "";
+
+            //save the user's name in sessionStorage
+            sessionStorage.setItem( 'username', name );
+
+            //reload room
+            location.reload();
+        }
+
+        else {
+            document.querySelector( '#err-msg-username' ).innerHTML = "Entrer votre nom s'il vous plait";
+        }
+    } );
+
+
+    document.addEventListener( 'click', ( e ) => {
+        if ( e.target && e.target.classList.contains( 'expand-remote-video' ) ) {
+            helpers.maximiseStream( e );
+        }
+
+        else if ( e.target && e.target.classList.contains( 'mute-remote-mic' ) ) {
+            helpers.singleStreamToggleMute( e );
+        }
+    } );
+
+
+    document.getElementById( 'closeModal' ).addEventListener( 'click', () => {
+        helpers.toggleModal( 'recording-options-modal', false );
+    } );
+} );
